650d2b0e301c76931e3759448ff9b45df8c124f0
maverickcalamari <trey.d.brown982@gmail.com>
2025-06-20 11:38:00 -0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // 👈 make sure this is imported
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 👈 this sets "@" to your /src folder
+    },
   },
-})
+});
